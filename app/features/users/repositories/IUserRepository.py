0e87e017dfb941fc9ec9b267e441c73d5e66eda5
Far14z <faridce14@gmail.com>
2025-09-23 01:25:10 -0500

    feat(user): add user class and repository contract.

---
+++ app/features/users/repositories/IUserRepository.py
@@ -0,0 +1,13 @@
+﻿from typing import Protocol, Optional
+from ..domain.User import User
+
+class IUserRepository(Protocol):
+
+    async def create(self, user: User) -> User:
+        ...
+
+    async def get_by_id(self, user_id: int) -> Optional[User]:
+        ...
+
+    async def get_by_email(self, email: str) -> Optional[User]:
+        ...
