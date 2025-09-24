a8a004e385eaeadbbc0443c6df0420a673513e02
Far14z <faridce14@gmail.com>
2025-09-23 22:54:09 -0500

    feat(user): add user creation endpoint and controller.

---
+++ app/features/users/presentation/controllers/users_controller.py
@@ -0,0 +1,21 @@
+﻿from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.features.users.application.handlers.commands.user_command_handler import UserCommandHandlerInterface
+from app.features.users.infrastructure.repository.sql_user_repository import SqlUserRepository
+from app.features.users.presentation.schemas.assemblers.create_user_command_from_resource_assembler import \
+    CreateUserCommandFromResourceAssembler
+from app.features.users.presentation.schemas.resources.create_user_resource import CreateUserResource
+from app.features.users.presentation.schemas.resources.user_response import UserResponse
+
+class UsersController:
+    def __init__(self, handler: UserCommandHandlerInterface):
+        self.handler = handler
+
+    async def create_user(self, payload: CreateUserResource) -> UserResponse:
+
+        try:
+            command = CreateUserCommandFromResourceAssembler.to_create_user_command(payload)
+            user = await self.handler.handle(command)
+            return UserResponse.model_validate(user)
+        except ValueError as e:
+            raise HTTPException(status_code=400, detail=str(e))
---
+++ app/features/users/presentation/routers/user_routers.py
@@ -0,0 +1,17 @@
+﻿from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database.database import get_db
+from app.features.users.application.handlers.commands.user_command_handler import UserCommandHandler
+from app.features.users.domain.handlers.user_command_handler_interface import UserCommandHandlerInterface
+from app.features.users.infrastructure.repository.sql_user_repository import SqlUserRepository
+from app.features.users.presentation.controllers.users_controller import UsersController
+from app.features.users.presentation.schemas.resources.create_user_resource import CreateUserResource
+from app.features.users.presentation.schemas.resources.user_response import UserResponse
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("", response_model=UserResponse, status_code=201)
+async def create_user(payload: CreateUserResource, db: AsyncSession = Depends(get_db)):
+    handler = UserCommandHandler(SqlUserRepository(db))
+    controller = UsersController(handler)
+    return await controller.create_user(payload)
